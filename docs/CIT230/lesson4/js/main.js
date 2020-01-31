f15c831e3290830f97d353eb383724b0a45bc8d0
Dukino <bro14078@byui.edu>
2020-01-31 15:51:12 -0700

    Fixed css files and footer dates

--- docs/CIT230/lesson4/js/main.js
+++ docs/CIT230/lesson4/js/main.js
@@ -1,13 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let lastModified = document.lastModified;
+document.addEventListener("DOMContentLoaded", function () {
     let year = new Date().getFullYear();
+    let d = new Date();
+    var months = ["January", "February", "March", "April", "May",
+        "June", "July", "August", "September", "October", "November",
+        "December"
+    ];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday"
+    ];
     document.getElementById('year').innerText = year;
-    document.getElementById("lastModified").innerText = lastModified;
+    document.getElementById("weekday").innerText = days[d.getDay()];
+    document.getElementById("fullDate").innerText = d.getDate() + " " +
+        months[d.getMonth()] + " " + year;
 });
 
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
+    document.querySelector(".navigation").classList.toggle("responsive");
 }
